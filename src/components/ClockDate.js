525c07f8f93656bfb87c601c0cf4802110ca03da
Carlos Sevilla <sevillecarlos@gmail.com>
2021-08-27 00:14:51 -0600

    fix the issue of the timer start/pause

--- src/components/ClockDate.js
+++ src/components/ClockDate.js
@@ -6,7 +6,15 @@ import "./style/ClockDate.css";
 
 const ClockDate = () => {
   const MODES = ["ALARM", "TIMER"];
-  const COLORS = ["#ff8600", "#ff0a54", "#0aefff", "#e71d36"];
+  const COLORS = [
+    "#ff0a54",
+    "#0aefff",
+    "#db00b6",
+    "#ff4800",
+    "#a5be00",
+    "#80FFDB",
+    "#f5dfbb",
+  ];
   const ALARM_DIGITS = [
     "HOUR_FIRST_DIGIT",
     "HOUR_SECOND_DIGIT",
@@ -15,19 +23,38 @@ const ClockDate = () => {
     "END",
   ];
 
+  const TIMER_DIGITS = [
+    "MINUTE_FIRST_DIGIT",
+    "MINUTE_SECOND_DIGIT",
+    "SECONDS_FIRST_DIGIT",
+    "SECONDS_SECOND_DIGIT",
+    "END",
+  ];
+
+  let interval = null;
+
   const [convert, setConvert] = useState(0);
   const [date, setDate] = useState("");
   const [modeCounter, setModeCounter] = useState(0);
   const [colorCounter, setColorCounter] = useState(0);
 
   const [alarmDigitCounter, setAlarmDigitCounter] = useState(0);
+  const [timerDigitCounter, setTimerDigitCounter] = useState(0);
 
   const [showDotsS, setShowDotsS] = useState(false);
   const [mode, setMode] = useState("");
 
   const [turnAlarmDigit, setTurnAlarmDigit] = useState("");
+  const [turnTimerDigit, setTurnTimerDigit] = useState("");
+
+  const [startTimer, setStartTimer] = useState(false);
+  const [stopTimer, setStopTimer] = useState(false);
+  const [counterTimer, setCounterTimer] = useState(0);
 
   const [playAlarm, setPlayAlarm] = useState(false);
+
+  const [digitsTimerSeconds, setDigitsTimerSeconds] = useState(0);
+  const [digitsTimerMinutes, setDigitsTimerMinutes] = useState(0);
 
   const showDots = useRef();
 
@@ -43,6 +70,14 @@ const ClockDate = () => {
     hourSecondDigit: 0,
     minuteFirstDigit: 0,
     minuteSecondDigit: 0,
+  });
+
+  const [timer, setTimer] = useState({
+    minuteFirstDigit: 0,
+    minuteSecondDigit: 0,
+    secondsFirstDigit: 0,
+    secondsSecondDigit: 0,
+    nanoSeconds: 59,
   });
 
   const clock = () => {
@@ -66,22 +101,21 @@ const ClockDate = () => {
     setShowDotsS(showDots.current);
 
     const alarmTime = localStorage.getItem("alarmtime");
-    const pausealarm = localStorage.getItem("pausealarm");
-    if (alarmTime) {
-      const clockTime = `${hour}:${minutes}`;
-      if (alarmTime === clockTime) {
-        // if (pausealarm) {
-        //   setPlayAlarm(false);
-        //   return;
-        // }
 
-        console.log(alarmTime);
-//here is the issue of the delay on the alarm SET TIME  button
-        // setPlayAlarm(true);
-      } else {
-        // setPlayAlarm(false);
-        // localStorage.removeItem("pausealarm");
+    const clockTime = `${hour}:${minutes}`;
+    checkSetAlarm(clockTime, alarmTime);
+  };
+
+  const checkSetAlarm = (clockTime, alarmTime) => {
+    const pausealarm = localStorage.getItem("pausealarm");
+    if (alarmTime === clockTime) {
+      if (pausealarm) {
+        setPlayAlarm(false);
+        return;
       }
+      setPlayAlarm(true);
+    } else {
+      // localStorage.removeItem("pausealarm");
     }
   };
 
@@ -120,6 +154,17 @@ const ClockDate = () => {
     });
 
     setTurnAlarmDigit(ALARM_DIGITS[alarmDigitCounter]);
+  };
+
+  const changeDigitTimer = () => {
+    setTimerDigitCounter((prevState) => {
+      if (prevState >= TIMER_DIGITS.length - 1) {
+        return (prevState = 0);
+      }
+      return prevState + 1;
+    });
+
+    setTurnTimerDigit(TIMER_DIGITS[timerDigitCounter]);
   };
 
   const stopAlarm = () => {
@@ -166,12 +211,107 @@ const ClockDate = () => {
     }
   };
 
+  const setTimeTimer = () => {
+    if (turnTimerDigit === "MINUTE_FIRST_DIGIT") {
+      setTimer((prevState) => {
+        if (timer.minuteFirstDigit > 5) timer.minuteFirstDigit = 0;
+        return { ...prevState, minuteFirstDigit: timer.minuteFirstDigit++ };
+      });
+    }
+
+    if (turnTimerDigit === "MINUTE_SECOND_DIGIT") {
+      setTimer((prevState) => {
+        if (timer.minuteSecondDigit > 9) timer.minuteSecondDigit = 0;
+        return { ...prevState, minuteSecondDigit: timer.minuteSecondDigit++ };
+      });
+    }
+    if (turnTimerDigit === "SECONDS_FIRST_DIGIT") {
+      setTimer((prevState) => {
+        if (timer.secondsFirstDigit > 5) timer.secondsFirstDigit = 0;
+        return { ...prevState, secondsFirstDigit: timer.secondsFirstDigit++ };
+      });
+    }
+
+    if (turnTimerDigit === "SECONDS_SECOND_DIGIT") {
+      setTimer((prevState) => {
+        if (timer.secondsSecondDigit > 9) timer.secondsSecondDigit = 0;
+        return { ...prevState, secondsSecondDigit: timer.secondsSecondDigit++ };
+      });
+    }
+  };
+
+  const setTimerStart = () => {
+    setStartTimer(true);
+    setStopTimer(true);
+    const joinSecondsDigits = parseInt(
+      `${timer.secondsFirstDigit}${timer.secondsSecondDigit}`
+    );
+
+    const joinMinutesDigits = parseInt(
+      `${timer.minuteFirstDigit}${timer.minuteSecondDigit}`
+    );
+    setDigitsTimerSeconds(joinSecondsDigits);
+    setDigitsTimerMinutes(joinMinutesDigits);
+
+    const timerInterval = setInterval(
+      () => {
+        setCounterTimer((prevState) => {
+          return prevState - 1;
+        });
+
+        const pauseTimer = localStorage.getItem("pausetimer");
+
+        if (pauseTimer) {
+          clearInterval(timerInterval);
+          localStorage.removeItem("pausetimer");
+        }
+      },
+
+      20
+    );
+  };
+
+  const pauseTimer = () => {
+    setStopTimer(false);
+    localStorage.setItem("pausetimer", true);
+  };
+
+  useEffect(() => {
+    if (timer.nanoSeconds === 0) {
+      setDigitsTimerSeconds((prevState) => {
+        return prevState - 1;
+      });
+      timer.nanoSeconds = 59;
+    }
+
+    if (digitsTimerSeconds === 0) {
+      setDigitsTimerSeconds(59);
+      setDigitsTimerMinutes((prevState) => {
+        return prevState - 1;
+      });
+    }
+
+    if (startTimer) timer.nanoSeconds--;
+
+    setTimer((prevState) => {
+      return {
+        ...prevState,
+        minuteFirstDigit: Number(String(formatTime(digitsTimerMinutes))[0]),
+        minuteSecondDigit: Number(String(formatTime(digitsTimerMinutes))[1]),
+        secondsFirstDigit: Number(String(formatTime(digitsTimerSeconds))[0]),
+        secondsSecondDigit: Number(String(formatTime(digitsTimerSeconds))[1]),
+      };
+    });
+  }, [counterTimer]);
+
+  useEffect(() => {}, []);
+
   useEffect(() => {
     const timeInterval = setInterval(clock, 1000);
     return () => {
       clearInterval(timeInterval);
     };
-  }, []);
+  });
 
   useEffect(() => {
     if (turnAlarmDigit === "END") {
@@ -182,9 +322,24 @@ const ClockDate = () => {
         minuteSecondDigit,
       } = alarm;
       const alarmTime = `${hourFirstDigit}${hourSecondDigit}:${minuteFirstDigit}${minuteSecondDigit}`;
+
       localStorage.setItem("alarmtime", alarmTime);
     }
   }, [turnAlarmDigit, alarm]);
+
+  useEffect(() => {
+    if (turnTimerDigit === "END") {
+      const joinSecondsDigits = parseInt(
+        `${timer.secondsFirstDigit}${timer.secondsSecondDigit}`
+      );
+
+      const joinMinutesDigits = parseInt(
+        `${timer.minuteFirstDigit}${timer.minuteSecondDigit}`
+      );
+      setDigitsTimerSeconds(joinSecondsDigits);
+      setDigitsTimerMinutes(joinMinutesDigits);
+    }
+  }, [turnTimerDigit, timer]);
 
   return (
     <div className="clock">
@@ -268,6 +423,66 @@ const ClockDate = () => {
               </div>
             </>
           )}
+          {mode === "TIMER" && (
+            <div className="timer">
+              {startTimer && turnTimerDigit === "END" ? (
+                <>
+                  <span>{formatTime(digitsTimerMinutes)}</span>
+                  <span>:</span>
+                  <span>{formatTime(digitsTimerSeconds)}</span>
+                </>
+              ) : (
+                <>
+                  {" "}
+                  <span
+                    style={{
+                      color:
+                        turnTimerDigit === "MINUTE_FIRST_DIGIT"
+                          ? "rgb(68, 68, 61)"
+                          : "",
+                    }}
+                  >
+                    {timer.minuteFirstDigit}
+                  </span>
+                  <span
+                    style={{
+                      color:
+                        turnTimerDigit === "MINUTE_SECOND_DIGIT"
+                          ? "rgb(68, 68, 61)"
+                          : "",
+                    }}
+                  >
+                    {timer.minuteSecondDigit}
+                  </span>
+                  <span>:</span>
+                  <span
+                    style={{
+                      color:
+                        turnTimerDigit === "SECONDS_FIRST_DIGIT"
+                          ? "rgb(68, 68, 61)"
+                          : "",
+                    }}
+                  >
+                    {timer.secondsFirstDigit}
+                  </span>
+                  <span
+                    style={{
+                      color:
+                        turnTimerDigit === "SECONDS_SECOND_DIGIT"
+                          ? "rgb(68, 68, 61)"
+                          : "",
+                    }}
+                  >
+                    {timer.secondsSecondDigit}
+                  </span>
+                </>
+              )}
+
+              <span className="nano-second-timer">
+                {formatTime(timer.nanoSeconds)}
+              </span>
+            </div>
+          )}
         </div>
         {modeCounter === 0 && (
           <button
@@ -305,6 +520,44 @@ const ClockDate = () => {
               </button>
             </>
           ))}
+        {mode === "TIMER" && (
+          <>
+            {!stopTimer && (
+              <button
+                className="timer-time-btn confirm"
+                style={{ color: COLORS[colorCounter] }}
+                onClick={changeDigitTimer}
+              >
+                Change
+              </button>
+            )}
+            {turnTimerDigit !== "END" ? (
+              <button
+                className="timer-time-btn set-time"
+                style={{ color: COLORS[colorCounter] }}
+                onClick={setTimeTimer}
+              >
+                Set time
+              </button>
+            ) : stopTimer ? (
+              <button
+                className="timer-time-btn pause-time"
+                style={{ color: COLORS[colorCounter] }}
+                onClick={pauseTimer}
+              >
+                Pause
+              </button>
+            ) : (
+              <button
+                className="timer-time-btn set-time"
+                style={{ color: COLORS[colorCounter] }}
+                onClick={setTimerStart}
+              >
+                Start
+              </button>
+            )}
+          </>
+        )}
       </div>
       {playAlarm && (
         <ReactAudioPlayer src={alarmSound} loop autoPlay={playAlarm} />
